86f157bf1bb73eacab81ceef84fcdec24af5ef7a
Akshay Memane <akshaymemane29@gmail.com>
2024-11-10 15:42:01 +0530

    added camera and enemy with animation

--- main.lua
+++ main.lua
@@ -1,19 +1,25 @@
 function love.load()
+    love.window.setMode(1000, 768)
+
     wf = require 'libs/windfield'
-    anim8 = require 'libs/anim8/anim8'
+    anim8 = require 'libs/anim8'
+    sti = require 'libs/sti'
+    cameraFile = require 'libs/camera'
+
+    cam = cameraFile()
 
     sprites = {}
     sprites.playerSheet = love.graphics.newImage('sprites/playerSheet.png')
-
+    sprites.enemySheet = love.graphics.newImage('sprites/enemySheet.png')
     local grid = anim8.newGrid(614, 564, sprites.playerSheet:getWidth(), sprites.playerSheet:getHeight())
+    local enemyGrid = anim8.newGrid(100, 79, sprites.enemySheet:getWidth(), sprites.enemySheet:getHeight())
 
     animations = {}
     animations.idle = anim8.newAnimation(grid('1-15', 1), 0.05)
     animations.jump = anim8.newAnimation(grid('1-7', 2), 0.05)
     animations.run = anim8.newAnimation(grid('1-15', 3), 0.05)
+    animations.enemy = anim8.newAnimation(enemyGrid('1-2', 1), 0.03)
 
-    W = love.graphics.getWidth()
-    H = love.graphics.getHeight()
     world = wf.newWorld(0, 500, false)
     world:setQueryDebugDrawing(true)
 
@@ -23,22 +29,34 @@ function love.load()
     -- world:addCollisionClass('Player', {ignores = {'Platform'}})
 
     require ('player')
-    platform = world:newRectangleCollider(250, 400, 300, 100, {collision_class = 'Platform'})
-    platform:setType('static')
+    require ('enemy')
 
-    dangerZone = world:newRectangleCollider(0, 550, 800, 50, {collision_class = 'Danger'})
-    dangerZone:setType('static')
+    -- dangerZone = world:newRectangleCollider(0, 550, 800, 50, {collision_class = 'Danger'})
+    -- dangerZone:setType('static')
+
+    platforms = {}
+
+    loadMap()
+    spawnEnemy(500, 320)
 end
 
 function love.update(dt)
     world:update(dt)
+    gameMap:update(dt)
     playerUpdate(dt)
+    updateEnemies(dt)
+
+    local px, py = player:getPosition()
+    cam:lookAt(px, love.graphics.getHeight()/2)
 end
 
 function love.draw()
-    world:draw()
-    drawPlayer()
-
+    cam:attach()
+        gameMap:drawLayer(gameMap.layers['Tile Layer 1'])
+        world:draw()
+        drawPlayer()
+        drawEnemies()
+    cam:detach()
 end
 
 function love.keypressed(key)
@@ -52,5 +70,22 @@ end
 function love.mousepressed(x, y, button)
     if button == 1 then
         local colliders = world:queryCircleArea(x, y, 200)
+    end
+end
+
+function loadMap()
+    gameMap = sti('maps/level1.lua')
+
+    for i, obj in pairs(gameMap.layers['Platforms'].objects) do
+        spawnPlatform(obj.x, obj.y, obj.width, obj.height)
+    end
+end
+
+function spawnPlatform(x, y, width, height)
+    if width > 0 and height > 0 then
+        local platform = world:newRectangleCollider(x, y, width, height, {collision_class = 'Platform'})
+        platform:setType('static')
+
+        table.insert(platforms, platform)
     end
 end
